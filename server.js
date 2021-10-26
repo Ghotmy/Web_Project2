bed696f72b941197268cbc5ca6a232d110899ca6
Ibrahim ELGhotmi <elghotmy7@gmail.com>
2021-10-26 23:02:31 +0200

    final touches

--- server.js
+++ server.js
@@ -1,19 +1,46 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
+const port=3000;
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser')
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const server = app.listen(port, listening);
+ function listening(){
+    console.log(server);
+    console.log(`running on localhost: ${port}`);
+  };
+
+// GET route
+app.get('/Wether', viewData);
+
+function viewData (request, response) {
+  response.send(projectData);
+};
+
+// Post route
+app.post('/addEntry',addData);
+
+function addData (request,response) {
+  // projectData.push(request.body);
+  projectData={...request.body};
+  response.end();
+};
--- website/app.js
+++ website/app.js
@@ -1,5 +1,66 @@
-/* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const apiKey="d586526d704de8b02857530558bd8bee";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getDate()+'.'+(d.getMonth()+1) +'.'+ d.getFullYear();
+
+// Event listener to add function to existing HTML DOM element
+const generateBtn= document.getElementById("generate");
+generateBtn.addEventListener("click",getTempretureData);
+
+// Function called by event listener
+async function getTempretureData(){
+
+    let zipCode = document.getElementById("zip").value;
+    let userFeel = document.getElementById("feelings").value;
+    let WetherUrl=`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=metric`;
+
+    getAPIData(WetherUrl)
+    .then((temp)=>{postData(newDate,temp,userFeel)})
+    .then(()=>{return getData()})
+    .then((finalResult)=>{
+        upadeUI(finalResult);
+    })
+    .catch(err => console.log(err))
+
+}
+/* Function to GET Web API Data*/
+async function getAPIData(url){
+    const res = await fetch(url);
+    const wetherJson = await res.json();
+    return wetherJson.main.temp;
+}
+
+/* Function to POST data */
+async function postData(date,temp,content){
+    await fetch("/addEntry",{
+        method:"POST",
+        credentials:"same-origin",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            date,
+            temp,
+            content
+        })
+    })
+}
+
+/* Function to GET Project Data */
+async function getData(){
+    const result = await fetch('/Wether');
+    const finalResult = await result.json();
+    return finalResult;
+    // console.log(finalResult);
+}
+
+/* Function to update UI */
+async function upadeUI(data){
+    // console.log(data);
+    document.getElementById('date').innerHTML=`Date: ${data.date}`;
+    document.getElementById('temp').innerHTML=`Temperature: ${data.temp}`;
+    document.getElementById('content').innerHTML=`Feels like: ${data.content}`;
+    document.getElementById('resultHolder').style="visibility:visible";
+}
